166c15591204ab163a801691a24b0d6a493502f6
Raja-Sadif <130021196+Raja-Sadif@users.noreply.github.com>
2023-05-31 00:08:57 -0700

    Update Assignment 5.js

--- Assignment 5.js
+++ Assignment 5.js
@@ -1,5 +1,1 @@
-// list of categories
-const categories = ['Lunch', 'Dinner', 'Sushi', 'Drinks'];
-<a href="#" onclick="$dc.loadMenuItems('${randomCategoryShortName}');">Specials</a>
-// generate random category short_name
-const randomCategoryShortName = categories[Math.floor(Math.random() * categories.length)];
+
